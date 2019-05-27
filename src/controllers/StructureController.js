afb490544eea00b81075c4a56307e57974ef16c7
João Tomé <joaotf159@gmail.com>
2019-05-26 22:37:29 -0300

    models

--- src/controllers/StructureController.js
+++ src/controllers/StructureController.js
@@ -1,36 +1,39 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
-const Structure = mongoose.model("Structure")
+const Structure = mongoose.model("Structure");
 
 module.exports = {
-    async findall(req,res){
-        const structures = await Structure.find();
-        
-        
-        return res.json(structures);
-    },
-    
-    async findone(req,res){
-        const structure = await Structure.findById(req.params.id);
-        return res.json(structure)
-    },
+  async findall(req, res) {
+    const structures = await Structure.find();
 
-    async store(req,res){
-       await 
-       Structure.create({
-            title: req.body.nome,
-            description: req.body.description
-        });
-        res.send("Usuário cadastrado com sucesso!")
-    },
+    return res.json(structures);
+  },
 
-    async update(req,res){
-        const structure = await Structure.findByIdAndUpdate(req.params.id,req.body,{new : true})
-        res.send("Usuário atualizado com sucesso!") 
-    },
+  async findone(req, res) {
+    const structure = await Structure.findById(req.params.id);
+    return res.json(structure);
+  },
 
-    async destroy(req,res){
-        await Structure.findByIdAndRemove(req.params.id)
-        res.send("Usuário excluído com sucesso!")
-    }
-}
+  async store(req, res) {
+    await Structure.create({
+      title: req.body.nome,
+      description: req.body.description,
+      date: req.body.date
+    });
+    res.send("Usuário cadastrado com sucesso!");
+  },
+
+  async update(req, res) {
+    const structure = await Structure.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+    res.send("Usuário atualizado com sucesso!");
+  },
+
+  async destroy(req, res) {
+    await Structure.findByIdAndRemove(req.params.id);
+    res.send("Usuário excluído com sucesso!");
+  }
+};
